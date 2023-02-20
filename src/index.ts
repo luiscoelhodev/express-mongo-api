f82bc18e8e20d8d48e064636c6b6a1a7e8fc1131
Luis Coelho <mancoeluis@gmail.com>
2023-02-20 15:10:48 -0300

    chore: add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database connection established successfully.');
+  })
+  .catch((error) => {
+    console.log('Database connection error.', error);
+  });
+
+app.get('/', (_req, res) => {
+  return res.json({ hello: 'world' });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
 });
