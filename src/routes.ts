f648679a9bae20cc87e13b00ddbb121a4465b05b
Luis Coelho <mancoeluis@gmail.com>
2023-02-22 10:22:10 -0300

    feat: add first routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import { UsersController } from './users-controller';
 const route = Router();
+const usersController = new UsersController();
 
 route.get('/', (_req, res) => {
   return res.json({ hello: 'world' });
 });
+
+route.get('/users', usersController.getUsers);
+
+route.post('/user', usersController.createUser);
 
 export { route };
