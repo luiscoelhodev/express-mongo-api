6c6dce7db5f83b58f4925516ab062192fb1f05ac
Luis Coelho <mancoeluis@gmail.com>
2023-02-22 14:11:45 -0300

    feat: add duplication handling

--- src/users-controller.ts
+++ src/users-controller.ts
@@ -13,15 +13,31 @@ export class UsersController {
     if (isBodyValid.success) {
       const newUser = new User(isBodyValid.data);
 
+      //Queries DB to check if user already exists
+      try {
+        const userAlreadyExists = await User.findOne({
+          $or: [{ email: newUser.email }, { ssn: newUser.ssn }],
+        });
+        if (userAlreadyExists) {
+          return response.status(400).json({ error: 'User already exists!' });
+        }
+      } catch (error) {
+        console.log(error);
+        return response.status(500).json({
+          message: 'Error in querying DB to check if user already exists.',
+        });
+      }
+
       try {
         await newUser.save();
         return response.status(201).json({
           message: 'User created successfully.',
         });
       } catch (error) {
+        console.log(error);
         return response
           .status(500)
-          .json({ message: 'Error in creating user.', error });
+          .json({ message: 'Error in creating user.' });
       }
     } else {
       return response.status(422).json({
